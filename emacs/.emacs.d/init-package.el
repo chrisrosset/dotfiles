69ec10bc448511812b0483db2b0ddc0abf4635e0
Christopher Rosset <chris@rosset.org.uk>
2014-07-27 01:59:18 +0100

    emacs: setting up some basic settings

---
+++ emacs/.emacs.d/init-package.el
@@ -0,0 +1,36 @@
+;; init-package.el
+;;
+;; This file is used for initializing package specific settings.
+
+;; evil
+;; - vi emulation mode
+(with-library evil
+  (setq evil-want-C-u-scroll t)
+  (require 'evil)
+  (evil-mode 1))
+
+;; fiplr
+;; - fuzzy file search within version control
+(with-library fiplr
+  (global-set-key (kbd "C-x f") 'fiplr-find-file))
+
+;; magit
+(with-library magit
+  (global-set-key (kbd "C-c m") 'magit-status))
+
+;; rainbow-delimiters
+;; - colorize pairs of brackets with different colors
+(with-library rainbow-delimiters
+  (global-rainbow-delimiters-mode))
+
+;; smartparents
+;; - smart handling of parentheses
+(with-library smartparens
+  (require 'smartparens-config)
+  (smartparens-global-mode t))
+
+;; yasnippet
+;; - defining and expanding snippets
+(with-library yasnippet
+  (yas-global-mode 1))
+
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,1 +1,27 @@
+;; Emacs initialization file
 
+;;;; Plugin / package setup
+
+
+(load-file "~/.emacs.d/init-package.el")
+
+;;;; Functions
+
+;; Form for executing code only if a particular library is available
+;; source: http://www.emacswiki.org/emacs/LoadingLispFiles
+(defmacro with-library (symbol &rest body)
+  `(condition-case nil
+       (progn
+	 (require ',symbol)
+	 ,@body)
+     (error (message (format "I guess we don't have %s available." ',symbol))
+	    nil)))
+(put 'with-library 'lisp-indent-function 1)
+
+;;;; General Keybindings
+
+(global-set-key (kbd "C-c f") 'ff-find-other-file)
+(global-set-key (kbd "C-c c") (lambda ()
+				(interactive)
+				(pop-to-buffer "*compilation*")
+				(compile (read-string "Compile command: " "make -k" ))))
