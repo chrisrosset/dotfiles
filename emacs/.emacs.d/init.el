b88a99e393f99c69466b1a45903405e5125ba091
Christopher Rosset <chris@rosset.org.uk>
2014-07-30 05:25:01 -0400

    emacs: rewrote the with-library function

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,15 +8,11 @@
 ;;;; Functions
 
 ;; Form for executing code only if a particular library is available
-;; source: http://www.emacswiki.org/emacs/LoadingLispFiles
 (defmacro with-library (symbol &rest body)
-  `(condition-case nil
-       (progn
-         (require ',symbol)
-         ,@body)
-     (error (message (format "I guess we don't have %s available." ',symbol))
-            nil)))
-(put 'with-library 'lisp-indent-function 1)
+  `(if (require ',symbol nil t)
+      (progn
+        ,@body)
+    (message (format "I guess we don't have %s available." ',symbol))))
 
 ;;;; General Settings
 
