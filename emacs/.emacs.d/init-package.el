5d8db953c9e770c5599ef991aa262e9d99bf91d9
Christopher Rosset <chris@rosset.org.uk>
2015-04-08 05:24:13 -0400

    emacs: change helm keybinding

--- emacs/.emacs.d/init-package.el
+++ emacs/.emacs.d/init-package.el
@@ -58,7 +58,7 @@
   (global-set-key (kbd "M-x")     'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
   (global-set-key (kbd "C-x C-r") 'helm-recentf)
-  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (global-set-key (kbd "C-x b")   'helm-buffers-list)
   (helm-mode 1))
 
 ;; magit
